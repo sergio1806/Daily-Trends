236c609f5ffe3769b3665e4e379ff8177f403fc5
sergio1806 <39763985+sergio1806@users.noreply.github.com>
2018-05-30 13:09:07 +0200

    Tarea 2
    Modelo feed y clase para crear objeto  de acceso a la base de datos

---
+++ Modelo/Feed.php
@@ -0,0 +1,128 @@
+
+<?php
+
+// Clase feed donse se cargaran los datos de las noticias
+class Feed {
+
+    private $pdo;
+   public $id;
+   public $titulo;
+   public $descripcion;
+   public $imagen;
+   public $fuente;
+   public $periodico;
+
+public function __CONSTRUCT()
+	{
+		try
+		{
+			$this->pdo = Database::StartUp();     
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Listar()
+	{
+		try
+		{
+			$result = array();
+
+			$stm = $this->pdo->prepare("SELECT * FROM feed");
+			$stm->execute();
+
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		}
+		catch(Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Obtener($id)
+	{
+		try 
+		{
+			$stm = $this->pdo
+			          ->prepare("SELECT * FROM feed WHERE id = ?");
+			          
+
+			$stm->execute(array($id));
+			return $stm->fetch(PDO::FETCH_OBJ);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Eliminar($id)
+	{
+		try 
+		{
+			$stm = $this->pdo
+			            ->prepare("DELETE FROM feed WHERE id = ?");			          
+
+			$stm->execute(array($id));
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Actualizar($data)
+	{
+		try 
+		{
+			$sql = "UPDATE feed SET 
+						titulo          = ?, 
+						descripcion        = ?,
+                                                imagen        = ?,
+						fuente            = ?, 
+						periodico = ?
+				    WHERE id = ?";
+
+			$this->pdo->prepare($sql)
+			     ->execute(
+				    array(
+                        $data->titulo, 
+                        $data->descripcion,
+                        $data->imagen,
+                        $data->fuente,
+                        $data->periodico,
+                        $data->id
+					)
+				);
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Registrar(Feed $data)
+	{
+		try 
+		{
+		$sql = "INSERT INTO feed (titulo,descripcion,imagen,fuente,periodico) 
+		        VALUES (?, ?, ?, ?, ?)";
+
+		$this->pdo->prepare($sql)
+		     ->execute(
+				array(
+                        $data->titulo, 
+                        $data->descripcion,
+                        $data->imagen,
+                        $data->fuente,
+                        $data->periodico
+                                      )
+                               );
+		} catch (Exception $e) 
+		{
+			die($e->getMessage());
+		}
+	}
+
+} 
+
+?>
